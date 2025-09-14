39fbbccda5a79babda179a45a50f5c1605793079
morrryyyy <morrryyyy20@gmail.com>
2025-09-14 17:13:51 +0100

    print a string to the console

---
+++ 0-javascript_is_amazing.js
@@ -0,0 +1,7 @@
+// Write a script that prints “JavaScript is amazing”:
+
+//     You must create a constant variable called myVar with the value “JavaScript is amazing”
+const myVar = "Javascript is amazing";
+//     You must use console.log(...) to print all output
+console.log(myVar);
+//     You are not allowed to use var
