788ba94518e474aac80659ad313c277b3dcf2847
morrryyyy <morrryyyy20@gmail.com>
2025-09-16 17:10:10 +0100

    print a string x times

---
+++ 7-multi_c.js
@@ -0,0 +1,19 @@
+// Write a script that prints x times “C is fun”
+
+//     Where x is the first argument of the script
+//     If the first argument can’t be converted to an integer, print “Missing number of occurrences”
+//     You must use console.log(...) to print all output
+//     You are not allowed to use var
+//     You can use only two console.log
+//     You must use a loop (while, for, etc.)
+const process = require("node:process");
+const argv = process.argv;
+
+argv[2] = Number(argv[2]);
+if (Number.isNaN(argv[2]) == true) {
+  console.log("Missing number of occurrences");
+} else {
+  for (let i = 0; i < argv[2]; i++) {
+    console.log("C is fun");
+  }
+}
