d6298e55e8e5529941ab675328ed2c9c46b4291a
morrryyyy <morrryyyy20@gmail.com>
2025-09-16 16:48:03 +0100

    print three strings in an array

---
+++ 6-multi_languages_loop.js
@@ -0,0 +1,17 @@
+// Write a script that prints 3 lines: (like 1-multi_languages.js) but by using an array of string and a loop
+
+//     The first line: “C is fun”
+//     The second line: “Python is cool”
+//     The third line: “JavaScript is amazing”
+//     You must use console.log(...) to print all output
+//     You are not allowed to use var
+//     You are not allowed to use any if/else statement
+//     You can use only one console.log
+//     You must use a loop (while, for, etc.)
+let str1 = "C is fun";
+let str2 = "Python is cool";
+let str3 = "JavaScript is amazing";
+let arr = [str1, str2, str3];
+for (let i = 0; i < arr.length; i++) {
+  console.log(arr[i]);
+}
