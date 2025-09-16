360f3d115904cf70b579b59c65f7afb16213c4ce
morrryyyy <morrryyyy20@gmail.com>
2025-09-16 10:29:30 +0100

    check how many arguments are passed

---
+++ 2-arguments.js
@@ -0,0 +1,17 @@
+// Write a script that prints a message depending of the number of arguments passed:
+
+//     If no arguments are passed to the script, print “No argument”
+//     If only one argument is passed to the script, print “Argument found”
+//     Otherwise, print “Arguments found”
+//     You must use console.log(...) to print all output
+//     You are not allowed to use var
+const process = require("node:process");
+const argv = process.argv;
+
+if (argv.length == 2) {
+  console.log("No argument");
+} else if (argv.length == 3) {
+  console.log("Argument found");
+} else {
+  console.log("Arguments found");
+}
